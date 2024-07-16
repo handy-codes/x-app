df6955c60a65b53b109565d0b4965bd31bca04ce
Emeka <paxymek@gmail.com>
2024-07-16 15:59:24 +0100

    complete Twitter-X-like App

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3ibrHETgGolDGx-ns1nhi6VexlmLVSWI",
+  authDomain: "owo-xplus.firebaseapp.com",
+  projectId: "owo-xplus",
+  storageBucket: "owo-xplus.appspot.com",
+  messagingSenderId: "618181706053",
+  appId: "1:618181706053:web:0f3ff762cba317ab0a3e2c"
+};
+
+// Initialize Firebase storage
+export const app = initializeApp(firebaseConfig);
