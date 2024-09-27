fb4eb94da8265070560408602c2957040e9d476c
emeka <emekaowo@yahoo.com>
2024-09-27 11:21:11 +0100

    fix2

--- src/components/SessionWrapper.jsx
+++ src/components/SessionWrapper.jsx
@@ -1,14 +1,14 @@
-'use client';
+// 'use client';
 
-import { SessionProvider } from 'next-auth/react';
-import { RecoilRoot } from 'recoil';
+// import { SessionProvider } from 'next-auth/react';
+// import { RecoilRoot } from 'recoil';
 
-const SessionWrapper = ({ children }) => {
-  return (
-    <SessionProvider>
-      <RecoilRoot>{children}</RecoilRoot>
-    </SessionProvider>
-  );
-};
+// const SessionWrapper = ({ children }) => {
+//   return (
+//     <SessionProvider>
+//       <RecoilRoot>{children}</RecoilRoot>
+//     </SessionProvider>
+//   );
+// };
 
-export default SessionWrapper;
+// export default SessionWrapper;
