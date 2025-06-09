d094cd1b44bcc7a1a659a26f2ee81be695429c47
Chukwuemeka U. Owo <158498788+handy-codes@users.noreply.github.com>
2025-06-09 04:04:41 +0100

    Fix timeago to handle Firestore timestamps

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,46 +1,50 @@
-// "use client"
+"use client"
 
-// import Link from 'next/link';
-// import { HiDotsHorizontal } from 'react-icons/hi';
-// import Icons from './Icons';
-// import ReactTimeAgo from 'react-time-ago';
-// import TimeAgo from 'javascript-time-ago'
-// import en from 'javascript-time-ago/locale/en'
-// import ru from 'javascript-time-ago/locale/ru'
+import Link from 'next/link';
+import { HiDotsHorizontal } from 'react-icons/hi';
+import Icons from './Icons';
+import ReactTimeAgo from 'react-time-ago';
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+import ru from 'javascript-time-ago/locale/ru'
 
-// TimeAgo.addDefaultLocale(en)
-// TimeAgo.addLocale(ru)
+TimeAgo.addDefaultLocale(en);
+TimeAgo.addLocale(ru);
 
-// // const time = Date.now()
-// const time = new Date().getTime()
+export default function Post({ post, id }) {
+  // Convert Firestore Timestamp to JS Date
+  const postDate = post.timestamp?.toDate ? post.timestamp.toDate() : new Date(post.timestamp);
 
+  return (
+    <div className='flex p-3 border-b border-gray-200 hover:bg-gray-50'>
+      <img
+        src={post?.profileImg}
+        alt='user-img'
+        className='h-11 w-11 rounded-full mr-4'
+      />
+      <div className='flex-1'>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center space-x-1 whitespace-nowrap'>
+            <h4 className='font-bold text-sm truncate'>{post?.name}</h4>
+            <span className='text-xs truncate'>@{post?.username}</span>
+            <span className='text-xs text-gray-500'>
+              <ReactTimeAgo date={postDate} locale='en-US' />
+            </span>
+          </div>
+          <HiDotsHorizontal className='text-sm' />
+        </div>
 
-// export default function Post({ post, id }) {
-//   return (
-//     <div className='flex p-3 border-b border-gray-200 hover:bg-gray-50'>
-//       <img
-//         src={post?.profileImg}
-//         alt='user-img'
-//         className='h-11 w-11 rounded-full mr-4'
-//       />
-//       <div className='flex-1'>
-//         <div className='flex items-center justify-between'>
-//           <div className='flex items-center space-x-1 whitespace-nowrap'>
-//             <h4 className='font-bold text-sm truncate'>{post?.name}</h4>
-//             <span className='text-xs truncate'>@{post?.username}</span>
-//             <span><ReactTimeAgo date={time} locale='en-US'/></span>
-//           </div>
-//           <HiDotsHorizontal className='text-sm' />
-//         </div>
+        <Link href={`/posts/${id}`}>
+          <p className='text-gray-800 text-sm my-3'>{post?.text}</p>
+        </Link>
+        {post?.image && (
+          <Link href={`/posts/${id}`}>
+            <img src={post.image} alt="post image" className='rounded-2xl mr-2' />
+          </Link>
+        )}
+        <Icons id={id} uid={post.uid} />
+      </div>
+    </div>
+  );
+}
 
-//         <Link href={`/posts/${id}`}>
-//           <p className='text-gray-800 text-sm my-3'>{post?.text}</p>
-//         </Link>
-//         <Link href={`/posts/${id}`}>
-//           <img src={post?.image} className='rounded-2xl mr-2' />
-//         </Link>
-//         <Icons id={id} uid={post.uid} />
-//       </div>
-//     </div>
-//   );
-// }
